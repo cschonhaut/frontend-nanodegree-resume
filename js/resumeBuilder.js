40e7df06b270d8c9f22c6f1905144711b7515567
Chelsea <cschonhaut@gmail.com>
2016-05-14 19:17:58 -0400

    Added Internationalize button

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,22 @@
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x,y);
+});
+
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() +
+		name[0].slice(1).toLowerCase();
+	return name[0] +" "+name[1];
+}
+
+$('#main').append(internationalizeButton);
+
 var bio = {
-	"name": "Chelsea",
+	"name": "Chelsea Schonhaut",
 	"role": "Designer/Developer",
 	"contacts": {
 		"email": "cschonhaut@gmail.com",
@@ -19,44 +36,45 @@ var work = {
 "jobs": [{
 	"employer": "Graphcom Creative",
 	"title": "Graphic Design Intern",
-	"dates": "January 2014-May 2014",
-	//"city": "Gettysburg",
+	"dates": "January 2014 - May 2014",
+	"location": "Gettysburg",
 	"description": "As a graphic design intern during my senior year of college, I quickly learned that I was not only in the right field, but also working at the right company. For 5 months I assisted the design and development teams with CMS data entry, branding projects, advertisements, magazine design, and much more. Fortunately I was offered a position at the company before graduation, and I am still there today."
 	},
 	{
 	"employer": "Graphcom Creative",
 	"title": "Creative Administrator",
-	"dates": "May 2014-August 2014",
-	//"city": "Gettysburg",
+	"dates": "May 2014 - August 2014",
+	"location": "Gettysburg",
 	"description": "Upon graduating college, there were no Junior Designer positions available at the company. However, Graphcom was looking for a Creative Administrative Assistant, and my knowledge of the company, organization skills, and leadership potential landed me the job. As Creative Administrator I oversaw a team of interns; gained experience in project management, client communication, and organization; and continued to assist the design and development teams while gaining valuable experience. After only 4 months in the job, I was promoted to Junior Designer. "
 	},
 	{
 	"employer": "Graphcom Creative",
 	"title": "Junior Graphic Designer",
-	"dates": "August 2014-Present",
-	//"city": "Gettysburg",
+	"dates": "August 2014 - Present",
+	"location": "Gettysburg",
 	"description": "My responsibilities as a designer have grown tremendously since my time as an intern. I began as a print-heavy designer, focusing primarily on magazines. I managed up to 4-5 different publications at a time. Today I still oversee some magazine operations, but I am now expanding more into web design and development. My goal is to bridge the gap between the design and development teams, as well as make the company more profitable. In addition to expanding my skillset and responsibilities, my project portfolio has grown vastly as well. In the past 2 years I have worked on signage, branding, advertising, magazines, annual reports, emialers, landing pages, websites, and more."}]
 };
+function displayWork(){
+	for (job in work.jobs) {
+		//create new div for work experience
+		$("#workExperience").append(HTMLworkStart);
+		//concat employer and title
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
 
-for (job in work.jobs) {
-	//create new div for work experience
-	$("#workExperience").append(HTMLworkStart);
-	//concat employer and title
-var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-var formattedEmployerTitle = formattedEmployer + formattedTitle;
-$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
 
-var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-$(".work-entry:last").append(formattedDates);
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	$(".work-entry:last").append(formattedLocation);
 
-//var formattedCity = HTMLworkCity.replace("%data%", work.jobs[job].city);
-//$(".work-entry:last").append(formattedCity);
-
-var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-$(".work-entry:last").append(formattedDescription);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+	}
 }
-
+displayWork();
 var education = {
 "schools": [{
 	"name": "Shippensburg University",
