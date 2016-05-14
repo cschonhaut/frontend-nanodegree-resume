c476ed9b49f4e960f15958a1a99bb6b31619eabe
Chelsea <cschonhaut@gmail.com>
2016-05-14 15:14:21 -0400

    Work is working
    Successfully added all job details to work elements

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,35 +20,41 @@ var work = {
 	"employer": "Graphcom Creative",
 	"title": "Graphic Design Intern",
 	"dates": "January 2014-May 2014",
-	"city": "Gettysburg",
-	"description": "As a graphic design intern during my senior year of college, I quickly learned that I was in not only the right field, but also the right company. I assisted the design and development teams with CMS data entry, branding projects, advertisements, magazine design, and much more. Fortunately I was offered a position at the company upon my graduation, and I am still there today."
+	//"city": "Gettysburg",
+	"description": "As a graphic design intern during my senior year of college, I quickly learned that I was not only in the right field, but also working at the right company. For 5 months I assisted the design and development teams with CMS data entry, branding projects, advertisements, magazine design, and much more. Fortunately I was offered a position at the company before graduation, and I am still there today."
 	},
 	{
 	"employer": "Graphcom Creative",
 	"title": "Creative Administrator",
 	"dates": "May 2014-August 2014",
-	"city": "Gettysburg",
-	"description": "Upon graduating college, there were no Junior Designer positions available at the company. However, Graphcom was looking for a Creative Administrative Assistant, and my knowledge of the company, organization skills, and leadership potential landed me the job. As Creative Administrator I oversaw a team of interns; gained experience in project management, client communication, and organization; as well as continued to assist the design and development teams. After only 4 months in the job, I was promoted to Junior Designer. "
+	//"city": "Gettysburg",
+	"description": "Upon graduating college, there were no Junior Designer positions available at the company. However, Graphcom was looking for a Creative Administrative Assistant, and my knowledge of the company, organization skills, and leadership potential landed me the job. As Creative Administrator I oversaw a team of interns; gained experience in project management, client communication, and organization; and continued to assist the design and development teams while gaining valuable experience. After only 4 months in the job, I was promoted to Junior Designer. "
 	},
 	{
 	"employer": "Graphcom Creative",
 	"title": "Junior Graphic Designer",
 	"dates": "August 2014-Present",
-	"city": "Gettysburg",
-	"description": "My responsibilities as a designer have grown tremendously since my time as an intern. I began as a print-heavy designer, focusing primarily on magazines. I managed up to 4-6 at a time. Today I still oversee magazine operations, but I am now expanding more into web design and development. My goal is to bridge the gap between the design and development teams, as well as make the company more profitable. In addition to expanding my skillsets, my project portfolio has grown vastly as well. In the past 2 years I have worked on signage, branding, advertising, magazines, annual reports, emialers, landing pages, websites, and more."}]
+	//"city": "Gettysburg",
+	"description": "My responsibilities as a designer have grown tremendously since my time as an intern. I began as a print-heavy designer, focusing primarily on magazines. I managed up to 4-5 different publications at a time. Today I still oversee some magazine operations, but I am now expanding more into web design and development. My goal is to bridge the gap between the design and development teams, as well as make the company more profitable. In addition to expanding my skillset and responsibilities, my project portfolio has grown vastly as well. In the past 2 years I have worked on signage, branding, advertising, magazines, annual reports, emialers, landing pages, websites, and more."}]
 };
 
 for (job in work.jobs) {
+	//create new div for work experience
 	$("#workExperience").append(HTMLworkStart);
-
-var formattedEmployer = HTMLworkEmployer.replace
-	("%data%", work.jobs[job].employer);
-var formattedTitle = HTMLworkTitle.replace
-	("%data%", work.jobs[job].title);
+	//concat employer and title
+var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 var formattedEmployerTitle = formattedEmployer + formattedTitle;
+$(".work-entry:last").append(formattedEmployerTitle);
 
-$(".work-entry:last").append(
-	formattedEmployerTitle);
+var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+$(".work-entry:last").append(formattedDates);
+
+//var formattedCity = HTMLworkCity.replace("%data%", work.jobs[job].city);
+//$(".work-entry:last").append(formattedCity);
+
+var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+$(".work-entry:last").append(formattedDescription);
 }
 
 var education = {
@@ -93,12 +99,14 @@ $("#header").prepend(formattedName);
 $("#header").append(formattedBioPic);
 $("#header").append(formattedWelcomeMsg);
 
-var formattedContacts = [];
-formattedContacts.push(HTMLemail.replace("%data%", bio.contacts.email));
+//attempt 1 to make contacts show
+//var formattedContacts = [];
+//formattedContacts.push(HTMLemail.replace("%data%", bio.contacts.email));
 //formattedContacts.push(HTMLphone.replace("%data%", bio.contacts.phone));
 //formattedContacts.push(HTMLlinkedin.replace("%data%", bio.contacts.linkedin));
 //formattedContacts.push(HTMLgithub.replace("%data%", bio.contacts.github));
 //formattedContacts.push(HTMLbehance.replace("%data%", bio.contacts.behance));
+//more attempts
 /*var contacts = "Contacts";
 var formattedContacts = HTMLcontacts.replace("%data%", bio.contacts[0]);
 $ ("#contacts").append(formattedContacts);
@@ -113,6 +121,23 @@ $("#contacts").append(formattedContacts);
 formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[5]);
 $("#contacts").append(formattedContacts);*/
 
+//NUmber 3
+if (bio.contacts.length > 0) {
+	$("#header").append(HTMLcontactsStart);
+
+var formattedContacts = HTMLcontacts.replace("%data%", bio.contacts[0]);
+$ ("#contacts").append(formattedContacts);
+formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[1]);
+$ ("#contacts").append(formattedContacts);
+formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[2]);
+$ ("#contacts").append(formattedContacts);
+formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[3]);
+$("#contacts").append(formattedContacts)
+formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[4]);
+$("#contacts").append(formattedContacts);
+formattedSkill = HTMLcontacts.replace("%data%", bio.contacts[5]);
+$("#contacts").append(formattedContacts);}
+
 if (bio.skills.length > 0) {
 	$("#header").append(HTMLskillsStart);
 
@@ -123,7 +148,7 @@ $ ("#skills").append(formattedSkill);
 formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
 $ ("#skills").append(formattedSkill);
 formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-$("#skills").append(formattedSkill);};
+$("#skills").append(formattedSkill);}
 
 /*
 $(document).ready = function(){
